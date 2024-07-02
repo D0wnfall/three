fc6e50e54f224dd8134dac8bc29f591d6e7e7733
Bill <136879108+D0wnfall@users.noreply.github.com>
2024-07-02 16:33:28 +0700

    update maps

--- mainTest.js
+++ mainTest.js
@@ -17,7 +17,7 @@ class Main {
 
 
         //plane
-        const loader = new GLTFLoader().setPath('resources/Models/').load('maps.gltf', (gltf) => {
+        const loader = new GLTFLoader().setPath('resources/Models/').load('Scene.gltf', (gltf) => {
             gltf.scene.scale.setScalar(1);
             gltf.scene.position.set(0,0,0);
             gltf.scene.shadowMap = true;
